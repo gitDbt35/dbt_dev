8c24eab33daa2fcba82d989cf162468343c3e4f0
Vedantdeshpande1998@gmail.com <Vedantdeshpande1998@gmail.com>
2024-03-06 04:49:48 +0000

    order added

--- models/bill.sql
+++ models/bill.sql
@@ -1,7 +1,5 @@
-
-
-select o.order_id,sum(m.price) from {{ ref('orders')}} as o
-inner join {{ ref('menu') }}  as m
-on o.ITEM_ID = m.MENU_ITEM_ID
-group by 1 order by 1
-
+select o.order_id, sum(m.price)
+from {{ ref("orders") }} as o
+inner join {{ ref("menu") }} as m on o.item_id = m.menu_item_id
+group by 1
+order by 1
